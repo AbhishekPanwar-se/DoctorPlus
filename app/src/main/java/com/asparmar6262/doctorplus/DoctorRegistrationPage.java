2bbc3828bcfe9f3e39c74ce10311529c2ff9d12e
admin <asparmar6262@gmail.com>
2023-04-10 00:34:15 +0530

    after apply signin methods

--- app/src/main/java/com/asparmar6262/doctorplus/DoctorRegistrationPage.java
+++ app/src/main/java/com/asparmar6262/doctorplus/DoctorRegistrationPage.java
@@ -1,20 +1,41 @@
 package com.asparmar6262.doctorplus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class DoctorRegistrationPage extends AppCompatActivity {
+    EditText email,password;
+    Button register;
+    TextView hasAccount;
+    ProgressBar progressBar;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_registration_page);
 
-        TextView hasAccount = findViewById(R.id.hasAccount);
+        hasAccount = findViewById(R.id.hasAccount);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+        progressBar = findViewById(R.id.progressBar);
+        mAuth = FirebaseAuth.getInstance();
 
         hasAccount.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +45,41 @@ public class DoctorRegistrationPage extends AppCompatActivity {
             }
         });
 
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerNewUser();
+            }
+        });
+
+    }
+
+    private void registerNewUser() {
+        progressBar.setVisibility(View.VISIBLE);
+        String GivenEmail,GivenPassword;
+        GivenEmail = email.getText().toString().trim();
+        GivenPassword = password.getText().toString().trim();
+
+        if(TextUtils.isEmpty(GivenEmail)){
+            Toast.makeText(getApplicationContext(),"Please Enter Email",Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(GivenPassword)){
+            Toast.makeText(getApplicationContext(),"Please enter Password",Toast.LENGTH_SHORT).show();
+        }
+
+        mAuth.createUserWithEmailAndPassword(GivenEmail,GivenPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Registration Successful", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(DoctorRegistrationPage.this, MainActivity.class);
+                    startActivity(intent);
+                    progressBar.setVisibility(View.GONE);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Registration Failed ! Please try again later", Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/asparmar6262/doctorplus/LoginActivity.java
+++ app/src/main/java/com/asparmar6262/doctorplus/LoginActivity.java
@@ -1,25 +1,85 @@
 package com.asparmar6262.doctorplus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView noAccount;
+    EditText email,loginPassword;
+    ProgressBar progressBar;
+    Button loginBtn;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        TextView noAccount = findViewById(R.id.noAccount);
+        noAccount = findViewById(R.id.noAccount);
+        email = findViewById(R.id.email);
+        loginPassword = findViewById(R.id.loginPassword);
+        progressBar = findViewById(R.id.progressBar);
+        loginBtn = findViewById(R.id.loginBtn);
+        mAuth = FirebaseAuth.getInstance();
         noAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        String GivenEmail,GivenPassword;
+
+        GivenEmail = email.getText().toString().trim();
+        GivenPassword = loginPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(GivenEmail)){
+            Toast.makeText(getApplicationContext(),"please enter Email",Toast.LENGTH_SHORT).show();
+        }
+        if(TextUtils.isEmpty(GivenPassword)){
+            Toast.makeText(getApplicationContext(),"please enter Password",Toast.LENGTH_SHORT).show();
+        }
+
+        mAuth.signInWithEmailAndPassword(GivenEmail,GivenPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(),"Login Successful",Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    progressBar.setVisibility(View.GONE);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Login Failed! Please try later",Toast.LENGTH_LONG).show();
+                    progressBar.setVisibility(View.GONE);
+                }
             }
         });
     }
