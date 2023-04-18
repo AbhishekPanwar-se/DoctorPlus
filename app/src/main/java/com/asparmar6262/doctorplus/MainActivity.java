e7a98dddb4fc3194a3be85f9e9c1e4b062929f69
admin <asparmar6262@gmail.com>
2023-04-19 01:49:49 +0530

    after implementing bottom navbar

--- app/src/main/java/com/asparmar6262/doctorplus/MainActivity.java
+++ app/src/main/java/com/asparmar6262/doctorplus/MainActivity.java
@@ -1,14 +1,51 @@
 package com.asparmar6262.doctorplus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
+
+
+    HomeFragment homeFragment = new HomeFragment();
+    SearchFragment searchFragment = new SearchFragment();
+    NotificationFragment notificationFragment = new NotificationFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavBar);
+        bottomNavigationView.setOnItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,homeFragment).commit();
+                return true;
+
+            case R.id.search:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,searchFragment).commit();
+                return true;
+
+            case R.id.notification:
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,notificationFragment).commit();
+                return true;
+        }
+
+        return false;
     }
 }
